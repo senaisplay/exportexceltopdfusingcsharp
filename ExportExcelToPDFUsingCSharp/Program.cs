8842d7f45a9d6a0dc1cb864004acb9b5316cdc3c
SPlay <SPlay@DESKTOP-U92BQFG>
2018-12-17 19:19:24 -0200

    Add project files.

---
+++ ExportExcelToPDFUsingCSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportExcelToPDFUsingCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new Application();
+            var pathToOpen = @"C:\Users\SPlay\Documents\products.xlsx";
+            app.Workbooks.Open(pathToOpen);
+            app.ActiveWorkbook.ExportAsFixedFormat(XlFixedFormatType.xlTypePDF);
+            app.ActiveWorkbook.Close(false);
+            app.Quit();
+        }
+    }
+}
